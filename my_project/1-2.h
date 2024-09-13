270b90f05b959cd7dcd7ad1bbf2633f7cf265d12
cmn000 <36029000@qq.com>
2024-09-13 11:56:11 +0800

    Update 1-2.h

--- my_project/1-2.h
+++ my_project/1-2.h
@@ -4,28 +4,28 @@ int charge_coin(int totalcost) {
     int sum = 0, coin, change;
 
     while (sum < totalcost) {
-        printf("请输入投币金额（1, 2, 5元）: ");
+        printf("enter the change number（1, 2, 5yuan）: ");
         scanf("%d", &coin);
         if (coin != 1 && coin != 2 && coin != 5) {
-            printf("无效投币金额。\n");
+            printf("do not have such change。\n");
             continue;
         }
         sum += coin;
     }
 
     change = sum - totalcost;
-    printf("找零: %d元\n", change);
+    printf("return change: %dyuan\n", change);
     return change; // 返回找零金额
 }; 
 
 int ma () {
     int total, remain;
 
-    printf("请输入总金额: ");
+    printf("enter the sum cost: ");
     scanf(" %d", &total); // 
     remain = charge_coin(total); // 接收 charge_coin 函数返回的找零金额
 
-    printf("最终找零: %d元\n", remain); // 打印最终找零金额
+    printf("finnel change: %dyuan\n", remain); // 打印最终找零金额
 
     return 0;
 }
